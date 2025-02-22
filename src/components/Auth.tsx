05f4a65da0aad78aa8235b0f583602fd48c199c3
Ryan Regalado <ryanreg48@gmail.com>
2025-02-22 11:22:03 -0600

    feat(Auth): test google auth redirect to /workouts

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -146,7 +146,7 @@ export default function Auth() {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/sign-in`,
+          redirectTo: `${window.location.origin}/workouts`,
           queryParams: {
             access_type: 'offline',
             prompt: 'consent'
